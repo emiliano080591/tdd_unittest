65229f1e7dd1461ebc8e39c26bef94487721494d
emiliano080591 <emilianoruben91@gmail.com>
2021-10-12 18:17:01 -0500

    First commit

---
+++ tests/unit/blog_test.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from blog import Blog
+
+
+class BlogTest(TestCase):
+    def test_create_blog(self):
+        b = Blog('Test', 'Test Author')
+
+        self.assertEqual('Test', b.title)
+        self.assertEqual('Test Author', b.author)
+        self.assertListEqual([], b.posts)
+        self.assertEqual(0, len(b.posts))
+
+    def test_repr(self):
+        b = Blog('Test', 'Test Author')
+
+        self.assertEqual(b.__repr__(), 'Test by Test Author (0 posts)')
+
+    def test_repr_multiple_posts(self):
+        b = Blog('Test', 'Test Author')
+        b.posts = ['test']
+
+        self.assertEqual(b.__repr__(), f'{b.title} by {b.author} ({len(b.posts)} posts)')
