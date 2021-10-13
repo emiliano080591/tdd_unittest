1a69fbbbfdb3fa27aa508dad73e196e7efb16344
emiliano080591 <emilianoruben91@gmail.com>
2021-10-13 13:53:52 -0500

    ADD test to endpoint in flask

--- app.py
+++ app.py
@@ -1,59 +1,12 @@
-from blog import Blog
+from flask import Flask, jsonify
 
-MENU_PROMPT = 'Enter "c" to create a blog, "l" to list blogs, "r" to read one, "p" to create post, or "q" to quit: '
-POST_TEMPLATE = '''
--- {} --
-{}
-
-'''
-blogs = dict()
+app = Flask(__name__)
 
 
-def menu():
-    print_blogs()
-    selection = input(MENU_PROMPT)
-    while selection != 'q':
-        if selection == 'c':
-            ask_create_blog()
-        elif selection == 'l':
-            print_blogs()
-        elif selection == 'r':
-            ask_read_blog()
-        elif selection == 'p':
-            ask_create_post()
-        selection = input(MENU_PROMPT)
+@app.route('/')
+def home():
+    return jsonify({'message': 'Hello world'})
 
 
-def print_blogs():
-    for key, blog in blogs.items():
-        print('- {}'.format(blog))
-
-
-def ask_create_blog():
-    title = input('Enter your blog title:')
-    author = input('Enter your name:')
-
-    blogs[title] = Blog(title, author)
-
-
-def ask_read_blog():
-    title = input('Enter the blog title you want to read:')
-
-    print_posts(blogs[title])
-
-
-def print_post(post):
-    print(POST_TEMPLATE.format(post.title, post.content))
-
-
-def print_posts(blog):
-    for post in blog.posts:
-        print_post(post)
-
-
-def ask_create_post():
-    blog_name = input('Enter the blog title you want to write a post in:')
-    title = input('Enter your post title:')
-    content = input('Enter your pos content')
-
-    blogs[blog_name].create_post(title, content)
+if __name__ == '__main__':
+    app.run()
