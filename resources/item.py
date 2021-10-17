527f9e1c2af097079d0b1b68674d706734509434
emiliano080591 <emilianoruben91@gmail.com>
2021-10-17 09:48:52 -0500

    ADD item system tests

--- resources/item.py
+++ resources/item.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
 from models.item import ItemModel
 
 
@@ -13,6 +14,7 @@ class Item(Resource):
                         required=True,
                         help="Every item needs a store id.")
 
+    @jwt_required()
     def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
